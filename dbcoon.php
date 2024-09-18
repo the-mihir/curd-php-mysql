4e0f2b6fdcb6cf6d552a8c4971cb2ac6df13f168
Mihir Das <mihirkantho@gmail.com>
2024-09-18 17:29:07 +0600

    Initial commit

---
+++ dbcoon.php
@@ -0,0 +1,16 @@
+<?php
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', 'admin');
+define('DB', 'curd_oparation');
+
+
+$connection = mysqli_connect(HOST, USER, PASS, DB);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
+
+?>
